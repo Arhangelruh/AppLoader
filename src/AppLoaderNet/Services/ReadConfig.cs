a638370e1b67461863df7143f75161fd3c00a5ff
Ruh <ruh@tut.by>
2023-11-09 10:40:01 +0300

    init: add 1) config file model 2) service for work with json config, service for work with files

---
+++ src/AppLoaderNet/Services/ReadConfig.cs
@@ -0,0 +1,16 @@
+﻿using AppLoaderNet.Model;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace AppLoaderNet.Services
+{
+    internal class ReadConfig
+    {
+        public Property readFile()
+        {
+            string jsonString = File.ReadAllText("AppLoader.json");
+            var configs = JsonConvert.DeserializeObject<Property>(jsonString);
+            return configs;
+        }
+    }
+}
